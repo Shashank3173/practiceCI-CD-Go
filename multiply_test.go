2996d735d3882ac18d31ed2e9b4cb5b5e30e4a1b
shashank <shashankojha0108@gmail.com>
2025-07-25 09:44:41 +0530

    "third Commit"

--- multiply_test.go
+++ multiply_test.go
@@ -2,7 +2,7 @@ package main
 import "testing"
 func TestProduct(t *testing.T){
 	result:=Product(4,5)
-	expected:=21
+	expected:=20
 	if result!=expected{
 		t.Errorf("Expected %d but got %d",expected,result)
 	}
